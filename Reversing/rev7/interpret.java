1f931f9841d1a3e8bca94688a30a1260d235f72c
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-14 15:36:17 -0500

    Add encryption to rev7

--- Reversing/rev7/interpret.java
+++ Reversing/rev7/interpret.java
@@ -1,17 +1,27 @@
 
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.Arrays;
 import java.security.MessageDigest;
 
-// TODO: encrypt hashes so they decrypt, encrypt user input at runtime
-
 public class interpret {
-    public static String hash_1 = "d5c67e2fc5f5f155dff8da4bdc914f41"; // masterchief
-    public static int key_1 = 117;
-    public static String hash_2 = "264212deff89ade15661a59e7b632872d858f2c6"; // princesspeach
+    // public static String clear_1 = "masterchief";
+    // public static int key_1 = 117;
+    // public static String hash_2 = "264212deff89ade15661a59e7b632872d858f2c6";
+    // public static String clear_2 = "princesspeach";
+    // public static String hash_3 =
+    // "5ebb49e499a6613e832e433a2722edd0d2947d56fdb4d684af0f06c631fdf633";
+    // public static String clear_3 = "solidsnake";
+
+    public static String secret_1 = "d5c67e2fc5f5f155dff8da4bdc914f41";
+    public static int[] secret_2 = { 114, 118, 116, 114, 113, 114, 36, 37, 38, 38, 120, 121, 33, 36, 37, 113, 117, 118,
+            118, 113, 33, 117, 121, 37, 119, 34, 118, 115, 114, 120, 119, 114, 36, 120, 117, 120, 38, 114, 35, 118 };
+    public static int[] secret_3 = { 268, 348, 347, 347, 269, 256, 348, 269, 256, 256, 344, 271, 271, 264, 266, 348,
+            257, 266, 267, 348, 269, 266, 266, 344, 267, 270, 267, 267, 348, 349, 349, 265, 349, 267, 256, 269, 270,
+            349, 268, 271, 351, 349, 347, 269, 349, 271, 257, 269, 344, 351, 265, 351, 265, 271, 346, 271, 266, 264,
+            351, 349, 351, 271, 266, 266 };
+
     public static int key_2 = 64;
-    public static String hash_3 = "5ebb49e499a6613e832e433a2722edd0d2947d56fdb4d684af0f06c631fdf633"; // solidsnake
     public static int key_3 = 313;
 
     public static void main(String[] args) {
@@ -33,7 +43,7 @@ public class interpret {
 
         System.out.println(
                 "Let's try some hash cracking!! I'll go easy on you the first time. The first hash we are checking is this");
-        System.out.println("\t" + hash_1);
+        System.out.println("\t" + secret_1);
         System.out.print("Think you can crack it? If so give me the value that hashes to that!\n\t");
 
         Scanner scanner = new Scanner(System.in);
@@ -41,19 +51,21 @@ public class interpret {
 
         String hash = hash(input, "MD5");
 
-        if (hash.compareTo(hash_1) == 0) {
+        if (hash.compareTo(secret_1) == 0) {
             System.out.println("Nice work! One to go ... many to come");
 
             System.out.print(
                     "This next one you don't get to see, if you aren't already digging into the class file you may wanna try that out!\n\t");
             input = scanner.nextLine();
-            if (hash(input, "SHA1").compareTo(hash_2) == 0) {
+            if (hash(input, "SHA1").compareTo(decrypt(secret_2, key_2)) == 0) {
                 System.out.println("Nice work!");
 
                 System.out.print("Alright last one...\n\t");
                 input = scanner.nextLine();
 
-                if (hash(input, "SHA-256").compareTo(hash_3) == 0) {
+                String hash_3 = hash(input, "SHA-256");
+                int[] encrypt_3 = encrypt(hash_3, key_3);
+                if (Arrays.equals(encrypt_3, secret_3)) {
                     System.out.println("That's correct!");
                     scanner.close();
                     return true;
@@ -67,16 +79,23 @@ public class interpret {
 
     private static int[] encrypt(String input, int key) {
         int[] output = new int[input.length()];
+        // System.out.print("{");
         for (int i = 0; i < input.length(); i++) {
             output[i] = (input.charAt(i) ^ key);
+            System.out.print((int) output[i]);
+            // if (i + 1 < input.length()) {
+            // System.out.print(", ");
+            // }
         }
+        // System.out.println("};");
+
         return output;
     }
 
     private static String decrypt(int[] input, int key) {
         String output = "";
         for (int i = 0; i < input.length; i++) {
-            output += (input[i] ^ key);
+            output += (char) ((input[i] ^ key));
         }
         return output;
     }
@@ -113,4 +132,5 @@ public class interpret {
 
         return digest;
     }
+
 }
