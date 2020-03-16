3c1760c14795bca34e44b608cfd1505a25eb716e
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-16 12:29:25 -0500

    Update client

--- Reversing/rev3/secret/client/client.go
+++ Reversing/rev3/secret/client/client.go
@@ -4,27 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 // NOTE: update with correct IP when ready
-const host = "127.0.0.1:27015" // host for connection
+const host = "192.168.1.35:27015" // host for connection
 const sendBuf = "hello can you hear me?"
 const requestFlag = "can i have the flag?"
 
 func main() {
 	// connect to server
-	conn, _ := net.Dial("tcp", host)
 
-	// send to server
-	fmt.Fprintf(conn, sendBuf)
-	// wait for reply
-	message, _ := bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Message from server: " + message)
+	if len(os.Args) != 2 {
+		fmt.Println("usage: " + os.Args[0] + " IP:PORT")
+		return
+	}
 
-	// send to server
-	fmt.Fprintf(conn, requestFlag)
-	// wait for reply
-	_, _ = bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Message from server: " + "")
+	initial := "Establishing Connection ... \n"
+	fmt.Println(initial)
+
+	conn, err := net.Dial("tcp", os.Args[1])
+	if err == nil {
+		// send to server
+		fmt.Fprintf(conn, sendBuf)
+		// wait for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+
+		// send to server
+		fmt.Fprintf(conn, requestFlag)
+		// wait for reply
+		_, _ = bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + "")
+	} else {
+		fmt.Print("Could not connect to server please contact admin")
+	}
 
 }
