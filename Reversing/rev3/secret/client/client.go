7f004345143edc281c0caa5511bbfb06e7e7c180
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-16 11:28:17 -0500

    Update client file

--- Reversing/rev3/secret/client/client.go
+++ Reversing/rev3/secret/client/client.go
@@ -1,5 +1,3 @@
-// socket client for golang
-// https://golangr.com
 package main
 
 import (
@@ -8,25 +6,25 @@ import (
 	"net"
 )
 
-const host = "127.0.0.1:8000" // host for connection
+// NOTE: update with correct IP when ready
+const host = "127.0.0.1:27015" // host for connection
 const sendBuf = "hello can you hear me?"
 const requestFlag = "can i have the flag?"
 
 func main() {
 	// connect to server
 	conn, _ := net.Dial("tcp", host)
-	for {
-		// send to server
-		fmt.Fprintf(conn, sendBuf)
-		// wait for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
 
-		// send to server
-		fmt.Fprintf(conn, requestFlag)
-		// wait for reply
-		_, _ = bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + "")
+	// send to server
+	fmt.Fprintf(conn, sendBuf)
+	// wait for reply
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + message)
 
-	}
+	// send to server
+	fmt.Fprintf(conn, requestFlag)
+	// wait for reply
+	_, _ = bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + "")
+
 }
