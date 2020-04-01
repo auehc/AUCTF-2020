9cb572fd80c21da93e51b8b875a21009bddc847b
Jordan Sosnowski <jjs0029@auburn.edu>
2020-04-01 11:35:51 -0500

    Replace crypter with decrypter and encrypter

--- Reversing/Medium - Encrypted/decrypter.py
+++ Reversing/Medium - Encrypted/decrypter.py
@@ -1,10 +1,10 @@
-file = open("enc.out", "rb")
+file = open("purple_socks", "rb")
 
 info = file.read()
 
-out_file = open("enc", "+wb")
+out_file = open("dec", "+wb")
 for x in info:
     enc = (x ^ 78)
-    if x is 0:
+    if x is 0 or x is 78:
         enc = x
     out_file.write(enc.to_bytes(1, 'little'))
