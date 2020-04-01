6689a3e66a065e5b3a907ee6381f69b5e6e0c0a2
ctf <ctf@auctf.com>
2020-04-01 15:02:17 +0000

    add  build script

---
+++ build.sh
@@ -0,0 +1,2 @@
+sudo docker stop $(sudo docker ps -aq) && sudo docker container prune -f
+sudo docker run -dit -p 30024:80 --name web5 web5 &&  sudo docker-compose --compatibility up --build -d && echo "[+] Challenges Up" 
