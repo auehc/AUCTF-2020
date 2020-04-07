9deb709854edda103dc6988b920e703facc044f8
Jordan Sosnowski <37343577+nadrojisk@users.noreply.github.com>
2020-04-07 11:17:16 -0500

    Bug in build script, should build web5 not web3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-sudo docker build Web/web3 -t web3 && \
+sudo docker build Web/web5 -t web5 && \
 sudo docker build Web/web4 -t web4 && \
-sudo docker run -dit -p 30023:80 --name web3 web3 && \
+sudo docker run -dit -p 30025:80 --name web5 web5 && \
 sudo docker run -dit -p 30024:80 --name web4 web4 && \
 sudo docker-compose --compatibility up --build -d && \
 echo "[+] Challenges Built"
